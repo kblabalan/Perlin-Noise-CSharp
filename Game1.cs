80709c6588a72969c3643d7fa8b67d2ca95d976e
Kim Justin <kj.labalan@yahoo.com>
2020-01-22 20:43:15 -0700

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Noise;
+using System;
+using System.Threading.Tasks;
+
+namespace PerlinNoiseMono
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+
+        string hashStr;
+        int seed;
+        double offset_x, offset_y, offset_z, scale;
+        Color[] buffer;
+        Perlin perlin;
+        bool isCalculating;
+        Texture2D texture;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            hashStr = "hashbrown";
+            seed = hashStr.GetHashCode();
+            perlin = new Perlin(seed);
+            scale = 0.1d;
+            texture = new Texture2D(GraphicsDevice, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+            buffer = new Color[graphics.PreferredBackBufferWidth * graphics.PreferredBackBufferHeight];
+            isCalculating = false;
+            CalculateNoise();
+            base.Initialize();
+        }
+		
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var state = Keyboard.GetState();
+            var calculate = false;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || state.IsKeyDown(Keys.Escape))
+                Exit();
+            if ((state.IsKeyDown(Keys.LeftAlt) || state.IsKeyDown(Keys.RightAlt)) && state.IsKeyDown(Keys.Enter))
+                graphics.ToggleFullScreen();
+
+            if (state.IsKeyDown(Keys.W))
+            {
+                offset_y -= 1d;
+                calculate = true;
+            }
+            if (state.IsKeyDown(Keys.A))
+            {
+                offset_x -= 1d;
+                calculate = true;
+            }
+            if (state.IsKeyDown(Keys.S))
+            {
+                offset_y += 1d;
+                calculate = true;
+            }
+            if (state.IsKeyDown(Keys.D))
+            {
+                offset_x += 1d;
+                calculate = true;
+            }
+            if (state.IsKeyDown(Keys.Q))
+            {
+                scale -= 0.001d;
+                calculate = true;
+            }
+            if (state.IsKeyDown(Keys.E))
+            {
+                scale += 0.001d;
+                calculate = true;
+            }
+            if (state.IsKeyDown(Keys.R))
+            {
+                offset_z += 0.01d;
+                calculate = true;
+            }
+            if (state.IsKeyDown(Keys.F))
+            {
+                offset_z -= 0.01d;
+                calculate = true;
+            }
+
+            if (calculate && !isCalculating)
+                Task.Run(() => CalculateNoise());
+
+            base.Update(gameTime);
+        }
+
+        private void CalculateNoise()
+        {
+            isCalculating = true;
+            for (int y = 0; y < graphics.PreferredBackBufferHeight; y++)
+            {
+                for (int x = 0; x < graphics.PreferredBackBufferWidth; x++)
+                {
+                    double output = perlin.Noise((offset_x + x) * scale, (offset_y + y) * scale, offset_z);
+                    int percentToByte = Math.Abs((int)(255d * output));
+                    if (percentToByte < 8)
+                        buffer[x + (y * graphics.PreferredBackBufferWidth)] = Color.DarkBlue;
+                    else if (percentToByte < 16)
+                        buffer[x + (y * graphics.PreferredBackBufferWidth)] = Color.Blue;
+                    else if (percentToByte < 32)
+                        buffer[x + (y * graphics.PreferredBackBufferWidth)] = Color.Brown;
+                    else if (percentToByte < 128)
+                        buffer[x + (y * graphics.PreferredBackBufferWidth)] = Color.Green;
+                    else if (percentToByte < 192)
+                        buffer[x + (y * graphics.PreferredBackBufferWidth)] = Color.White;
+                }
+            }
+            isCalculating = false;
+            texture.SetData(buffer);
+            Window.Title = $"Perlin Noise - {hashStr} ({seed}) - Offset: [{offset_x},{offset_y},{offset_z}] Scale: {scale}";
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, Vector2.Zero, Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Noise/Perlin.cs
@@ -0,0 +1,87 @@
+﻿/*
+	Kenneth Perlin's 2002 Improved Noise
+	https://mrl.nyu.edu/~perlin/noise/
+	
+	C# Port by Kim Justin B. Labalan
+*/
+
+using System;
+
+namespace Noise
+{
+    public class Perlin
+    {
+        private Random random;
+        private int[] p;
+        public Perlin()
+        {
+            random = new Random();
+            p = new int[512];
+            Calculate();
+        }
+        public Perlin(int seed)
+        {
+            SetSeed(seed);
+            p = new int[512];
+            Calculate();
+        }
+
+        public void SetSeed(int seed)
+        {
+            random = new Random(seed);
+        }
+
+        public double Noise(double x, double y, double z)
+        {
+            int X = (int)Math.Floor(x) & 255,
+                Y = (int)Math.Floor(y) & 255,
+                Z = (int)Math.Floor(z) & 255;
+            x -= Math.Floor(x);
+            y -= Math.Floor(y);
+            z -= Math.Floor(z);
+            double u = Fade(x),
+                   v = Fade(y),
+                   w = Fade(z);
+            int A =  p[X] + Y,
+                AA = p[A] + Z,
+                AB = p[A + 1] + Z,
+                B =  p[X + 1] + Y,
+                BA = p[B] + Z,
+                BB = p[B + 1] + Z;
+            return Lerp(w, Lerp(v, Lerp(u, Gradient(p[AA], x, y, z),
+                                           Gradient(p[BA], x - 1, y, z)),
+                                   Lerp(u, Gradient(p[AB], x, y - 1, z),
+                                           Gradient(p[BB], x - 1, y - 1, z))),
+                           Lerp(v, Lerp(u, Gradient(p[AA + 1], x , y, z - 1),
+                                           Gradient(p[BA + 1], x - 1, y, z - 1)),
+                                   Lerp(u, Gradient(p[AB + 1], x , y - 1, z - 1),
+                                           Gradient(p[BB + 1], x - 1, y - 1, z - 1))));
+        }
+
+        private double Fade(double t)
+        {
+            return t * t * t * (t * (t * 6d - 15d) + 10d);
+        }
+
+        private double Lerp(double t, double a, double b)
+        {
+            return a + t * (b - a);
+        }
+
+        private double Gradient(int hash, double x, double y, double z)
+        {
+            int h = hash & 15;
+            double u = h < 8 ? x : y,
+                   v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+            return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+        }
+
+        private void Calculate()
+        {
+            for (int count = 0; count < 256; count++)
+            {
+                p[count] = p[count + 256] = random.Next(0, 256);
+            }
+        }
+    }
+}
